5b9c24426d9c141e700feb4646bfc04b8fe3a4b9
cody-junier <codyjunier@gmail.com>
2021-12-20 19:49:38 -0800

    Created table in server.js

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const db = mysql.createConnection(
 //     });
 // });
 
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.table(rows);
+})
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
